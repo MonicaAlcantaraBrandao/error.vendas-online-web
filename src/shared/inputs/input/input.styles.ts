bb1bebe989581e911b957d758ea4a952d76413c9
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-11-26 17:31:04 -0300

    create first global component

---
+++ src/shared/inputs/input/input.styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const BoxInput = styled.div`
+
+`
+
+export const TitleInput = styled.h3`
+    font-family: Poppins;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 21px;
+    letter-spacing: 0em;
+    text-align: left;
+    margin: 8px;
+
+`
