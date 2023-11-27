18aa3312a288999f580e5403c3959586271e0356
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-11-26 22:36:03 -0300

    insert useState in login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import Button from "../../../shared/buttons/button/Button";
 import Input from "../../../shared/inputs/input/input";
 import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage, TitleLogin } from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(event.target.value);
+  }
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  }
+
+  const handleLogin = () => {
+    alert(`${userName}, ${password}`)
+  }
+
   return (
     <ContainerLoginScreen>
       <BackgroundImage src="./background.png"/>
@@ -10,9 +26,9 @@ const LoginScreen = () => {
             <LimitedContainer>
             <LogoImage src="./logo.png"/>
             <TitleLogin level={2} type='secondary'>LOGIN</TitleLogin>
-            <Input title="USUÁRIO:"/>
-            <Input title="SENHA:"/>
-            <Button type="primary" margin="64px 0px 16px 0px" >ENTRAR</Button>
+            <Input margin="32px 0px 0px" title="USUÁRIO:" onChange={handleUserName} value={userName}/>
+            <Input type="password" margin="32px 0px 0px" title="SENHA:" onChange={handlePassword} value={password}/>
+            <Button type="primary" margin="64px 0px 16px 0px" onClick={handleLogin} >ENTRAR</Button>
             </LimitedContainer>
         </ContainerLogin>
     </ContainerLoginScreen>
--- src/shared/inputs/input/input.styles.ts
+++ src/shared/inputs/input/input.styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { Typography } from 'antd'
 
-const { Text} = Typography;
+const { Text } = Typography;
 
 export const BoxInput = styled.div`
     width: 100%;
