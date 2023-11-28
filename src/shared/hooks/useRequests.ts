8ae01815bfbcd34baeded624afc5b977e5987c7e
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-11-28 19:14:21 -0300

    create function connection

--- src/shared/hooks/useRequests.ts
+++ src/shared/hooks/useRequests.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react"
 import { useGlobalContext } from "./useGlobalContext";
+import { ConnectionAPIPost } from "../functions/connection/connectionAPI";
 
 export const useRequests = () => {
     const [loading, setLoading] = useState(false);
@@ -24,19 +25,15 @@ export const useRequests = () => {
         return returnData;
     };
 
-    const postRequest = async (url: string, body: any) => {
+    const postRequest = async (url: string, body: unknown) => {
         setLoading(true);
-        const returnData = await axios({
-            method: "post",
-            url: url,
-            data: body,
-          })
+        const returnData = await ConnectionAPIPost(url, body)
           .then((result) => {
             setNotification('Entrando...', 'success', 'aguarde')
-            return result.data
+            return result;
           })
-          .catch(() => {
-            setNotification('Usuário ou senha inválidos', 'error')
+          .catch((error: Error) => {
+            setNotification(error.message, 'error')
         });
 
         setLoading(false);
