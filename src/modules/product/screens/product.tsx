157cb0cc29dc2564dbdf4c448b70e59abc839440
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-11-29 16:39:55 -0300

    insert category in productTable

--- src/modules/product/screens/product.tsx
+++ src/modules/product/screens/product.tsx
@@ -4,23 +4,31 @@ import { URL_PRODUCT } from "../../../shared/constants/urls";
 import { MethodsEnum } from "../../../shared/enums/method.enum";
 import { useDataContext } from "../../../shared/hooks/useDataContext";
 import { useRequests } from "../../../shared/hooks/useRequests";
-import { ProductType } from "../types/ProductType";
+import { ProductType } from "../../../shared/types/ProductType";
 
 import type { ColumnsType } from "antd/es/table";
 import Table from "../../../shared/components/table/Table";
+import CategoryColumn from "../components/CategoryColumn";
+import TooltipImage from "../components/TooltipImage";
 
 const columns: ColumnsType<ProductType> = [
   {
     title: "Id",
     dataIndex: "id",
     key: "id",
-    render: (text) => <a>{text}</a>,
+    render: (_, product) => <TooltipImage product={product}/>
   },
   {
     title: "Nome",
     dataIndex: "name",
     key: "name",
     render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Categoria",
+    dataIndex: "category",
+    key: "category",
+    render: (_, product) => <CategoryColumn category={product.category}/>,
   },
   {
     title: "Preço",
