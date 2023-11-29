be3c34da45c1d6b7264c65be4599c415ca79fdb9
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-11-29 18:41:36 -0300

    create customized tooltip

--- src/modules/product/components/TooltipImage.tsx
+++ src/modules/product/components/TooltipImage.tsx
@@ -1,16 +1,17 @@
-import { Tooltip } from "antd";
-import { ProductType } from "../../../shared/types/ProductType";
+import Tooltip from '../../../shared/components/tooltip/Tooltip';
+import { ProductType } from '../../../shared/types/ProductType';
+import { ImageProduct } from '../styles/tooltipImage.style';
 
 interface TooltipImageProps {
-    product: ProductType;
+  product: ProductType;
 }
 
-const TooltipImage = ({product}: TooltipImageProps) => {
-    return (
-        <Tooltip title={product.name}>
-        <span>{product.id}</span>
-        </Tooltip>
-    )
-}
+const TooltipImage = ({ product }: TooltipImageProps) => {
+  return (
+    <Tooltip tooltip={<ImageProduct src={product.image} />}>
+      <span>{product.id}</span>
+    </Tooltip>
+  );
+};
 
 export default TooltipImage;
